38e4061d5cc66b6f27b2eb646383778330bb3b57
Nguyen Phuong Nam <nam.nguyenphuong@savvycomsoftware.com>
2021-01-14 14:10:28 +0700

    Change update open browser

--- index.js
+++ index.js
@@ -4,18 +4,186 @@ const browserObject = require('./browser');
 const scraperController = require('./pageController');
 
 //Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+let browserInstance;
+let page;
+(async () => {
+  browserInstance = await browserObject.startBrowser();
+  page = await browserInstance.newPage();
+  await page.goto("https://plus24.mbs.com.vn/apps/StockBoard/MBS/CW.html");
+  await page.waitForSelector("#boardData");
+})()
+
+async function crawl() {
+  let urls = await page.evaluate(() => {
+    let data = [];
+    for (let i = 0; i < $("#boardData").children().length; i += 2) {
+      const record = $("#boardData").children()[i];
+      const metrics = $("#boardData").find(record).children();
+      data.push({
+        "MaCQ": {
+          value: $("#boardData").find(metrics[0]).text().trim(),
+          title: "Mã CQ"
+        },
+        "ToChucPhatHanh": {
+          value: $("#boardData").find(metrics[1]).children().first().text().trim(),
+          title: "Tổ chức phát hành"
+        },
+        "NgayDaoHan": {
+          value: $("#boardData").find(metrics[1]).children().last().text().trim(),
+          title: "Ngày đáo hạn"
+        },
+        "Tran": {
+          value: $("#boardData").find(metrics[2]).text().trim(),
+          title: "Trần"
+        },
+        "San": {
+          value: $("#boardData").find(metrics[3]).text().trim(),
+          title: "Sàn"
+        },
+        "ThamChieu": {
+          value: $("#boardData").find(metrics[4]).text().trim(),
+          title: "Tham chiếu"
+        },
+        "DuMuaGia3": {
+          value: $("#boardData").find(metrics[5]).text().trim(),
+          title: "Dư mua - Giá 3"
+        },
+        "DuMuaKL3": {
+          value: $("#boardData").find(metrics[6]).text().trim(),
+          title: "Dư mua - KL 3"
+        },
+        "DuMuaGia2": {
+          value: $("#boardData").find(metrics[7]).text().trim(),
+          title: "Dư mua - Giá 2"
+        },
+        "DuMuaKL2": {
+          value: $("#boardData").find(metrics[8]).text().trim(),
+          title: "Dư mua - KL 2"
+        },
+        "DuMuaGia1": {
+          value: $("#boardData").find(metrics[9]).text().trim(),
+          title: "Dư mua - Giá 1"
+        },
+        "DuMuaKL1": {
+          value: $("#boardData").find(metrics[10]).text().trim(),
+          title: "Dư mua - KL 1"
+        },
+        "GiaKhop": {
+          value: $("#boardData").find(metrics[11]).text().trim(),
+          title: "Khớp lệnh - Giá"
+        },
+        "PhanTramThayDoi": {
+          value: $("#boardData").find(metrics[12]).children().first().text().trim(),
+          title: "Khớp lệnh - Phần trăn thay đổi %"
+        },
+        "PhanTramThayDoi+-": {
+          value: $("#boardData").find(metrics[12]).children().last().text().trim(),
+          title: "Khớp lệnh - Phần trăn thay đổi \+/\-"
+        },
+        "KLKhop": {
+          value: $("#boardData").find(metrics[13]).text().trim(),
+          title: "Khớp lệnh - KL"
+        },
+        "DuBanGia1": {
+          value: $("#boardData").find(metrics[14]).text().trim(),
+          title: "Dư bán - Giá 1"
+        },
+        "DuBanKL1": {
+          value: $("#boardData").find(metrics[15]).text().trim(),
+          title: "Dư bán - KL 1"
+        },
+        "DuBanGia2": {
+          value: $("#boardData").find(metrics[16]).text().trim(),
+          title: "Dư bán - Giá 2"
+        },
+        "DuBanKL2": {
+          value: $("#boardData").find(metrics[17]).text().trim(),
+          title: "Dư bán - KL 2"
+        },
+        "DuBanGia3": {
+          value: $("#boardData").find(metrics[18]).text().trim(),
+          title: "Dư bán - Giá 3"
+        },
+        "DuBanKL3": {
+          value: $("#boardData").find(metrics[19]).text().trim(),
+          title: "Dư bán - KL 3"
+        },
+        "TongGT": {
+          value: $("#boardData").find(metrics[20]).children().first().text().trim(),
+          title: "Tổng GT"
+        },
+        "TongKL": {
+          value: $("#boardData").find(metrics[20]).children().last().text().trim(),
+          title: "Tổng KL"
+        },
+        "Du_Mua": {
+          value: $("#boardData").find(metrics[21]).children().first().text().trim(),
+          title: "Dư - Mua"
+        },
+        "GiaCao": {
+          value: $("#boardData").find(metrics[21]).children().last().text().trim(),
+          title: "Giá - Cao"
+        },
+        "Du_Ban": {
+          value: $("#boardData").find(metrics[22]).children().first().text().trim(),
+          title: "Dư - Bán"
+        },
+        "GiaThap": {
+          value: $("#boardData").find(metrics[22]).children().last().text().trim(),
+          title: "Giá - Thấp"
+        },
+        "NDTNN_Mua": {
+          value: $("#boardData").find(metrics[23]).children().first().text().trim(),
+          title: "NĐTNN - Mua"
+        },
+        "CoSo_MaCK": {
+          value: $("#boardData").find(metrics[23]).children().last().text().trim(),
+          title: "Cơ sở - Mã CK"
+        },
+        "NDTNN_Ban": {
+          value: $("#boardData").find(metrics[24]).children().first().text().trim(),
+          title: "NĐTNN - Bán"
+        },
+        "CoSo_Gia": {
+          value: $("#boardData").find(metrics[24]).children().last().text().trim(),
+          title: "Cơ sở - Giá"
+        },
+        "TH": {
+          value: $("#boardData").find(metrics[25]).text().trim(),
+          title: "TH"
+        },
+        "DoLech": {
+          value: $("#boardData").find(metrics[26]).text().trim(),
+          title: "Độ lệch"
+        },
+        "TL_CD": {
+          value: $("#boardData").find(metrics[27]).text().trim(),
+          title: "TL CĐ"
+        },
+        "DHV": {
+          value: $("#boardData").find(metrics[28]).text().trim(),
+          title: "ĐHV"
+        },
+      })
+    }
+    return data
+  })
+  return urls
+}
+// let browserInstance = browserObject.startBrowser();
 
 app.get('/', async (req, res) => {
-    res.json(await scraperController(browserInstance));
+  res.json(await crawl());
 });
 
 app.get('*', async (req, res) => {
-    res.json({errorMessage: "404 Not Found"});
+  res.json({
+    errorMessage: "404 Not Found"
+  });
 });
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, function () {
-    console.log("Server is running port: 8080");
+  console.log("Server is running port: 8080");
 });
